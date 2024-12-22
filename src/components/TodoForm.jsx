facc40b1820987e51fa4344285d9ba18d6182b79
Bruno Cesar <83827977+brunoo85@users.noreply.github.com>
2024-12-22 14:03:42 -0300

    fix: excluindo bibliotecas desnecessarias

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,33 +1,43 @@
-import React from 'react'
+import React from 'react';
+import { useForm } from 'react-hook-form';
 
-const TodoForm = ({addTodo}) => {
-  const [value, setValue] = React.useState("");
-  const [prioridade, setPrioridade] = React.useState("");
+const TodoForm = ({ addTodo }) => {
+  const { register, handleSubmit, formState: { errors }, reset } = useForm({
+    defaultValues: {
+      text: '',
+      prior: '',
+    }
+  });
 
-  const handleSubmit = (e) =>{
-    e.preventDefault();
-    if(!value || !prioridade) return;
-    addTodo(value,prioridade);
-   setPrioridade("");
-   setValue("");
-  }
+  const onSubmit = (data) => {
+    addTodo(data.text, data.prior);
+    reset();
+  };
 
   return (
-    <div className='todo-form'>
-      <h2>Criar tarefa:</h2>
-      <form onSubmit={handleSubmit}>
-          <input type="text" placeholder='Digite o título' value={value} onChange={(e) => setValue(e.target.value)} />
-          <select value={prioridade} onChange={(e) => setPrioridade(e.target.value)}>
-               <option disabled value="">Selecione a prioridade</option>
-               <option value="Urgente">Urgente</option>
-               <option value="Alta">Alta</option>
-               <option value="Média">Média</option>
-               <option value="Baixa">Baixa</option>
-          </select>
-          <button type="submit">Criar Tarefa</button>
-      </form>
-    </div>
-  )
-}
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input
+        placeholder="Adicione uma tarefa"
+        {...register("text", { required: "Título é necessário" })}
+        aria-invalid={errors.text ? "true" : "false"}
+      />
+      {errors.text && <p role="alert">{errors.text.message}</p>}
+
+      <select 
+        {...register("prior", { required: "Prioridade é necessária" })}
+        aria-invalid={errors.prior ? "true" : "false"}
+      >
+        <option disabled value="" >Selecione a prioridade</option>
+        <option value="Urgente">Urgente</option>
+        <option value="Alta">Alta</option>
+        <option value="Média">Média</option>
+        <option value="Baixa">Baixa</option>
+      </select>
+      {errors.prior && <p role="alert">{errors.prior.message}</p>}
+
+      <input  className='bg-red-600' type="submit" value="Adicionar Tarefa"/>
+    </form>
+  );
+};
 
 export default TodoForm;
